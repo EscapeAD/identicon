d59c79292b3983ad0b6f50754c3c0088e5a9f645
Adam Tse <mr.adam.tse@gmail.com>
2017-07-16 23:29:03 -0400

    add pattern matching

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,7 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
   
   def hash_input(input) do
@@ -11,4 +12,10 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
   
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g , b | _tail] = hex_list
+    
+    [r, g, b]
+  end
 end
